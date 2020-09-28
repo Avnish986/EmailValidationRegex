c611d310eb335aad99c7a20b8cc7cb24ea1a9ab9
avnish986 <avnishgupta986@gmail.com>
2020-09-28 17:41:49 +0530

    [Avnish]Created:email validation

---
+++ src/com/email/validate/EmailValidation.java
@@ -0,0 +1,12 @@
+package com.email.validate;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+public class EmailValidation {
+
+	public static void main(String[] args) {
+		System.out.println("Email validation");
+
+	}
+
+}
